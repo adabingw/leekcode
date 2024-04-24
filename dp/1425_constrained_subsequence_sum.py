65867cc2d0cb567ecb9f4f9acd67d667baccb570
Ada W <adabingw@yahoo.com>
2024-04-24 16:29:15 -0400

    constrained-subsequence-sum.py commit

---
+++ dp/1425_constrained_subsequence_sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def constrainedSubsetSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = [(-nums[0], 0)]
+        ans = nums[0]
+        
+        for i in range(1, len(nums)):
+            while i - heap[0][1] > k:
+                heapq.heappop(heap)
+
+            curr = max(0, -heap[0][0]) + nums[i]
+            ans = max(ans, curr)
+            heapq.heappush(heap, (-curr, i))
+
+        return ans
