79e74fa605f2d4774017e372fe362fb8b2b839ad
Ada W <adabingw@yahoo.com>
2024-05-29 08:50:15 -0400

    sorting-three-groups.py commit

---
+++ dp/2826_sorting_three_groups.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [1] * (n)
+
+        for i in range(1, n):
+            for j in range(0, i):
+                if nums[j] <= nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return len(nums) - max(dp)
