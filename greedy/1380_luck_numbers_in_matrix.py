10d4f0923b0953e48c05c2afbf0481f13bf6bf8d
Ada W <adabingw@yahoo.com>
2024-07-19 18:52:52 -0400

    lucky-nums-in-matrix.py commit

---
+++ greedy/1380_luck_numbers_in_matrix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def luckyNumbers (self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        N, M = len(matrix), len(matrix[0])
+
+        r_min_max = float('-inf')
+        for i in range(N):
+            r_min = min(matrix[i])  
+            r_min_max = max(r_min_max, r_min)
+
+        c_max_min = float('inf')
+        for i in range(M):
+            c_max = max(matrix[j][i] for j in range(N))
+            c_max_min = min(c_max_min, c_max)
+
+        if r_min_max == c_max_min:
+            return [r_min_max]
+        else:
+            return []
+        
