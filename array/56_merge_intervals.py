e9c0fc3badec89a1b4972db2a7c6a24eae012cf4
Ada W <adabingw@yahoo.com>
2024-09-12 09:55:44 +0800

    merge-intervals.py commit

--- array/56_merge_intervals.py
+++ array/56_merge_intervals.py
@@ -4,22 +4,14 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        
-        intervals = sorted(intervals)
-        result = []
+        intervals.sort(key=lambda x: x[0])
 
-        curr_max = intervals[0][1]
-        curr_min = intervals[0][0]
-        
+        res = []
+
         for interval in intervals:
-            if interval[0] <= curr_max:
-                curr_max = max(curr_max, interval[1])
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
             else:
-                result.append([curr_min, curr_max])
-                curr_min = interval[0]
-                curr_max = interval[1]
-
-        result.append([curr_min, curr_max])
-
-        print(result)
-        return result
+                res[-1][1] = max(res[-1][1], interval[1])
+        
+        return res
