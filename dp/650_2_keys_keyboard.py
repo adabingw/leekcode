545321adf388680df2bd78ce1c69a479b2db04bc
Ada W <adabingw@yahoo.com>
2024-12-30 18:44:41 +0800

    2-keys-keyboard.py commit

---
+++ dp/650_2_keys_keyboard.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minSteps(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [1000] * (n + 1)
+        dp[1] = 0
+
+        for i in range(2, n + 1):
+            for j in range(1, i // 2 + 1):
+                if i % j == 0:
+                    dp[i] = min(dp[i], dp[j] + i // j)
+        
+        return dp[-1]
