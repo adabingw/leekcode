6ea8932c98d1fdd183eeb36c0514553fdc5ed7f4
Ada W <adabingw@yahoo.com>
2025-01-02 14:47:28 +0800

    reach-a-number.py commit

---
+++ math/754_reach_a_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reachNumber(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        target = abs(target)
+        k = 0
+        while target > 0:
+            k += 1
+            target -= k
+        
+        return k if target % 2 == 0 else k + 1 + k % 2
