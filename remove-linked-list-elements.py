a29b3e58a7fe0f281c4ca3ab15d7ec90493556f4
Ada W <adabingw@yahoo.com>
2025-01-12 18:35:01 -0500

    remove-linked-list-elements.py commit

---
+++ remove-linked-list-elements.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        if not head:
+            return head
+        head.next = self.removeElements(head.next, val)
+        return head.next if head.val == val else head
