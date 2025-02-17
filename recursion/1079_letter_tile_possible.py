bc10fc559e9d3d1837abc8e6682fd9502ce6a2fe
Ada W <adabingw@yahoo.com>
2025-02-16 19:59:20 -0500

    letter-tile-possibilities.py commit

---
+++ recursion/1079_letter_tile_possible.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numTilePossibilities(self, tiles):
+        sequences = set()
+        used = [False] * len(tiles)
+
+        # Generate all possible sequences including empty string
+        self._generate_sequences(tiles, "", used, sequences)
+
+        # Subtract 1 to exclude empty string from count
+        return len(sequences) - 1
+
+    def _generate_sequences(
+        self, tiles, current, used, sequences
+    ):
+        sequences.add(current)
+
+        # Try adding each unused character to current sequence
+        for pos, char in enumerate(tiles):
+            if not used[pos]:
+                used[pos] = True
+                self._generate_sequences(tiles, current + char, used, sequences)
+                used[pos] = False
+        
