e23bd0a4ba431ea837e9c56e6929f6fdb4810f09
Ada W <adabingw@yahoo.com>
2025-02-28 21:05:42 -0500

    apply-operations-to-an-array.py commit

---
+++ array/2460_apply_ops_to_an_array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def applyOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        res = []
+
+        for i in range(0, n - 1):
+            if nums[i] == nums[i + 1] and nums[i] != 0:
+                nums[i] *= 2
+                nums[i + 1] = 0
+            
+        for num in nums:
+            if num != 0:
+                res.append(num)
+        
+        while len(res) < n:
+            res.append(0)
+        
+        return res
